0a2f5528923b78a8e2bb699f0ae4578286d672e8
Marat <mrmar99@icloud.com>
2023-05-06 01:18:40 +0300

    Update data provider and Select components

---
+++ src/data/data.ts
@@ -0,0 +1,47 @@
+type Tower = {
+  id: string;
+  name: string;
+};
+
+type Floor = {
+  id: string;
+  towerId: string;
+  level: number;
+};
+
+type Room = {
+  towerId: string;
+  floorId: string;
+  number: number;
+};
+
+const generateRandomId = () => {
+  return Math.random().toString(36).slice(2, 9);
+}
+
+const towers: { [id: string]: Tower } = {
+  "towerA": { id: "towerA", name: "Башня А" },
+  "towerB": { id: "towerB", name: "Башня Б" }
+};
+
+const floors: { [id: string]: Floor } = {};
+Object.entries(towers).forEach(([key, value]) => {
+  for (let level = 3; level <= 27; level++) {
+    const floor: Floor = { id: generateRandomId(), towerId: key, level };
+    floors[floor.id] = floor;
+  }
+});
+
+const rooms: Room[] = [];
+Object.values(floors).forEach((floor) => {
+  for (let i = 1; i <= 10; i++) {
+    const room: Room = { towerId: floor.towerId, floorId: floor.id, number: i };
+    rooms.push(room);
+  }
+});
+
+export {
+  towers,
+  floors,
+  rooms
+}
